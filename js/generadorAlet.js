54b459ec16bd1814b01842f4c94aab7d8250ab80
alan <cloudubuntuwork@gmail.com>
2021-11-16 16:54:55 -0300

    se termino la funcion de generar un unico numero aleatorio

--- js/generadorAlet.js
+++ js/generadorAlet.js
@@ -2,42 +2,32 @@ let regCod = [];
 let repeat;
 
 function aleatorio() {
+  do {
+    let code = parseInt(Math.random() * 10);
 
-    
-        let code = parseInt(Math.random() * 10);
-
-        repeat=codeRepeat(code);
-        if(!repeat){
-            regCod.push(code);
-            console.log('aqui se guardo')
-            console.log(code);
-            return code;
-           
-        }else{
-            
-            console.log('aqui no se guardo');
-            console.log(code);
-            repeat=false;
-        }
-    
-        
- 
+    repeat = codeRepeat(code);
+    if (!repeat) {
+      regCod.push(code);
+      return code;
+    } else {
+      repeat = false;
+    }
+  } while (!repeat);
 }
 
 function codeRepeat(code) {
   for (let i = 0; i < regCod.length; i++) {
     if (code === regCod[i]) {
       repeat = true;
-        break;
+      break;
     }
-    
   }
   return repeat;
 }
 
-
- for(let i=0; i<10; i++){
+function btn(){
     let code=aleatorio();
- }
- console.log('--------------------------------')
- console.log(regCod);
+    console.log(code);
+    console.log(regCod);
+    console.log('-------------------');
+}
